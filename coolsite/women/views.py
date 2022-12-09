6b0fdce6f71269e828b530718bc7d1ff3a9842c5
Sasha Osin <sashaosin364@gmail.com>
2022-12-09 14:28:16 +0700

    Add orm db

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 def index(request):
     return HttpResponse("Главная страница")
+
+def pageNotFound(request,exeption):
+    return HttpResponseNotFound("Страница не найдена")
