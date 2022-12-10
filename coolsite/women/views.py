e30d53bfa250c4c894cd336f7bf00810070cef83
Sasha Osin <sashaosin364@gmail.com>
2022-12-10 16:44:37 +0700

    Add templates and static.Fix html

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound
 
-def index(request):
-    return HttpResponse("Главная страница")
+from .models import *
 
+menu = [{'title':'О сайте','url_name':'about'},
+        {'title':'Добавить статью','url_name':'add_page'},
+        {'title':'Обратная связь','url_name':'contact'},
+        {'title':'Войти','url_name':'login'}]
+def index(request):
+    posts = Women.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request,'women/index.html',context=context)
+
+def about(request):
+    return render(request,'women/about.html',{'menu':menu,'title':'О странице'})
+
+def addpage(request):
+    return HttpResponse("Добавление страницы")
+
+def contact(request):
+    return HttpResponse("Contact")
+
+def login(request):
+    return HttpResponse("Login")
+
+def show_post(request,post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 def pageNotFound(request,exeption):
     return HttpResponseNotFound("Страница не найдена")
