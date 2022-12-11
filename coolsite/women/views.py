94f49ca7e1b3098d39b0638fe2b38db2f2cec538
Sasha Osin <sashaosin364@gmail.com>
2022-12-11 22:02:37 +0700

    Add category

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from .models import *
 
@@ -9,10 +9,13 @@ menu = [{'title':'О сайте','url_name':'about'},
         {'title':'Войти','url_name':'login'}]
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats':cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected':0,
     }
     return render(request,'women/index.html',context=context)
 
@@ -32,3 +35,18 @@ def show_post(request,post_id):
     return HttpResponse(f'Отображение статьи с id = {post_id}')
 def pageNotFound(request,exeption):
     return HttpResponseNotFound("Страница не найдена")
+def show_category(request,cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats':cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected':cat_id,
+    }
+    return render(request,'women/index.html',context=context)
